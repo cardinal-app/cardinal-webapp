f6c1b1734f9f6b5b17b07cc706251387c2067025
jrsmth-tier2 <jsmith@tier2consulting.com>
2023-12-05 14:27:53 +0000

    Demo: add basic home screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import {HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NgbModule, RouterOutlet, FontAwesomeModule, RouterLink],
+  imports: [CommonModule, NgbModule, RouterOutlet, FontAwesomeModule, RouterLink, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
