7dec7108fbe56981658a0320a3a6d79cedcbc636
jrsmth <jamesrichardsmith97@gmail.com>
2023-12-30 11:33:21 +0000

    Basic login navigation

--- src/app/structure/login/login.component.ts
+++ src/app/structure/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
-import {RouterOutlet} from "@angular/router";
+import {Router, RouterOutlet} from "@angular/router";
 import {LandingComponent} from "../../core/component/landing/landing.component";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faLock, faUser} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingComponent, NgOptimizedImage],
+  imports: [CommonModule, RouterOutlet, LandingComponent, NgOptimizedImage, FaIconComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  protected readonly faUser = faUser;
+  protected readonly faLock = faLock;
+
+  constructor(private router: Router) {}
+
+  login() {
+    this.router.navigate(['/home']);
+  }
 
 }
