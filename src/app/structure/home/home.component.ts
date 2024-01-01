ceeb4267c650b77943ffe5547456ef74233b8cb6
jrsmth <jamesrichardsmith97@gmail.com>
2024-01-01 19:17:37 +0000

    Landing component colour updates

--- src/app/structure/home/home.component.ts
+++ src/app/structure/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {LandingComponent} from "../../core/component/landing/landing.component";
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LandingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
